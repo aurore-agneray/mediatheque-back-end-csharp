f3842df312c83245bf19fce31060f7f75e97b231
aurore-agneray <aurore.agn@outlook.fr>
2024-07-06 12:28:27 +0200

    Publication des fichiers de base de l'appli pour son fonctionnement, avec un controller et une classe d'entité "Author". Le controller renvoie une liste d'auteurs saisie "en dur"

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,32 @@
+﻿using mediatheque_back_csharp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediatheque_back_csharp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ILogger<AuthorController> _logger;
+
+        private static readonly Author[] Authors = new[]
+        {
+            new Author() { Code = "ABC", FirstName = "Yo !", LastName = "POUET", Id = 1 },
+            new Author() { Code = "taertgertg", FirstName = "Lola", LastName = "HEHE", Id = 2 },
+            new Author() { Code = "zefzef", FirstName = "Jean", LastName = "Michel", Id = 3 },
+            new Author() { Code = "rgzezer", FirstName = "Johnny", LastName = "Halliday", Id = 4 },
+            new Author() { Code = "zerzerz", FirstName = "Céline", LastName = "Dion", Id = 5 },
+        };
+
+        public AuthorController(ILogger<AuthorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAuthor")]
+        public IEnumerable<Author> Get()
+        {
+            return Authors;
+        }
+    }
+}
---
+++ Entities/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace mediatheque_back_csharp.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public String? Code { get; set; }
+        public String? FirstName { get; set; }
+        public String? LastName { get; set; }
+        public String? CompleteName { get; set; }
+    }
+}
