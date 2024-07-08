0056c9066485eecd049d7da1fa412f57d0b4d998
aurore-agneray <aurore.agn@outlook.fr>
2024-07-08 14:26:52 +0200

    Ajout de DTOs pour renvoyer les propriétés des livres, auteurs et genres souhaitées dans la recherche simple ou avancée + màj de SimpleSearchController pour faire appel à ces DTOs

---
+++ DTOs/SearchDTOs/BookResultDTO.cs
@@ -0,0 +1,62 @@
+﻿using mediatheque_back_csharp.Dtos;
+using mediatheque_back_csharp.Interfaces;
+
+namespace mediatheque_back_csharp.DTOs.SearchDTOs
+{
+    /// <summary>
+    /// Object retrieved for the Book while searching for books
+    /// </summary>
+    public class BookResultDTO : IIdentified
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Staff comment
+        /// </summary>
+        public string? StaffComment { get; set; }
+
+        /// <summary>
+        /// Author Result DTO
+        /// </summary>
+        public AuthorResultDTO? Author { get; set; }
+
+        /// <summary>
+        /// Genre Result DTO
+        /// </summary>
+        public GenreResultDTO? Genre { get; set; }
+
+        /// <summary>
+        /// Constructor of the BookResultDTO
+        /// </summary>
+        /// <param name="book">A BookDTO object</param>
+        public BookResultDTO(BookDTO book)
+        {
+            if (book != null)
+            {
+                Id = book.Id;
+                Title = book.Title;
+                StaffComment = book.StaffComment;
+                Author = new AuthorResultDTO();
+                Genre = new GenreResultDTO();
+
+                if (book.Author != null)
+                {
+                    Author.CompleteName = book.Author.CompleteName;
+                }
+
+                if (book.Genre != null) 
+                { 
+                    Genre.GenreName = book.Genre.Name;
+                }
+            }
+        }
+    }
+}
---
+++ DTOs/SearchDTOs/GenreResultDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace mediatheque_back_csharp.DTOs.SearchDTOs
+{
+    /// <summary>
+    /// Object retrieved for the Genre while searching for books
+    /// </summary>
+    public class GenreResultDTO
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string GenreName { get; set; } = null!;
+    }
+}
