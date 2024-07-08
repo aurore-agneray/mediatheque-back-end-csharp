eacd89cb6c9768dba5f6958d632cc0cf95ba5e3b
aurore-agneray <aurore.agn@outlook.fr>
2024-07-08 13:33:05 +0200

    Correction using dans SimpleSimpleController après merge de la branche develop

--- Controllers/SearchControllers/SimpleSearchController.cs
+++ Controllers/SearchControllers/SimpleSearchController.cs
@@ -1,4 +1,5 @@
-﻿using mediatheque_back_csharp.Interfaces;
+﻿using mediatheque_back_csharp.Database;
+using mediatheque_back_csharp.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
