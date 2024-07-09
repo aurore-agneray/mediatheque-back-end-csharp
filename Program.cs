a422cd71417a94a1ebdd5bb8482f9d51a2fdc43d
aurore-agneray <aurore.agn@outlook.fr>
2024-07-09 15:43:06 +0200

    Ajout des domaines front-end autorisés à appeler l'API dans le fichier appsettings.json + ajustements dans Program.cs pour pouvoir récupérer les domaines

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ using mediatheque_back_csharp.Database;
 // Creates a dependency injection container
 var builder = WebApplication.CreateBuilder(args);
 
+// Retrieves the configuration settings entered into the appsettings.json file
+builder.Services.Configure<MySettingsModel>(builder.Configuration.GetSection("MySettings"));
+var myAppSettings = builder.Configuration.GetSection("MySettings").Get<MySettingsModel>();
+
 // Add services to the container.
 builder.Services.AddControllers();
 
@@ -16,19 +20,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MediathequeDbContext>();
 
 // Configures CORS policy
+var frontDomainsStr = myAppSettings?.FrontEndDomains;
+string[] frontDomains = null;
+
+if (frontDomainsStr != null)
+{
+    frontDomains = frontDomainsStr.Split(';');
+}
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins(frontDomains ?? ["http://localhost:5173"])
                   .WithHeaders("Content-type")
                   .WithMethods("GET", "POST");
         });
 });
-
-// Retrieves the configuration settings entered into the appsettings.json file
-builder.Services.Configure<MySettingsModel>(builder.Configuration.GetSection("MySettings"));
 
 // Configures AutoMapper used for converting my POCOs into DTOs
 builder.Services.AddAutoMapper(cfg =>
