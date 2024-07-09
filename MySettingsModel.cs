df1872ccd018cb6a2d46b1eb65ffd3e7a1fd305f
aurore-agneray <aurore.agn@outlook.fr>
2024-07-09 15:43:06 +0200

    Ajout des domaines front-end autorisés à appeler l'API dans le fichier appsettings.json + ajustements dans Program.cs pour pouvoir récupérer les domaines

--- MySettingsModel.cs
+++ MySettingsModel.cs
@@ -9,5 +9,11 @@
         /// Connection string used for connecting to the database, that depends on the execution environment
         /// </summary>
         public string? DbConnectionString { get; set; }
+
+        /// <summary>
+        /// Domain(s) of the front-end that will call the API.
+        /// If several domains, separate them with ;
+        /// </summary>
+        public string? FrontEndDomains { get; set; }
     }
 }
