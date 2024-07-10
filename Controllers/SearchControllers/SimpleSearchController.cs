aa7006a2fce84227495aa7f5190b92f07354585c
aurore-agneray <aurore.agn@outlook.fr>
2024-07-10 09:32:56 +0200

    Déplacement du mapper de SimpleSearchController vers son Manager, de sorte à alléger le contenu du controller + déplacement des traitements des livres renvoyés par la base

--- Controllers/SearchControllers/SimpleSearchController.cs
+++ Controllers/SearchControllers/SimpleSearchController.cs
@@ -1,8 +1,6 @@
-﻿using AutoMapper;
-using mediatheque_back_csharp.Database;
+﻿using mediatheque_back_csharp.Database;
 using mediatheque_back_csharp.DTOs.SearchDTOs;
 using mediatheque_back_csharp.Managers.SearchManagers;
-using mediatheque_back_csharp.Pocos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,11 +25,6 @@ public class SimpleSearchController : ControllerBase
     protected readonly ILogger<SimpleSearchController> _logger;
 
     /// <summary>
-    /// Transforms the POCOs into DTOs
-    /// </summary>
-    protected readonly IMapper _mapper;
-
-    /// <summary>
     /// Methods for preparing the data sent by the SimpleSearchController
     /// </summary>
     public readonly SimpleSearchManager _manager;
@@ -41,17 +34,15 @@ public class SimpleSearchController : ControllerBase
     /// </summary>
     /// <param name="context">Given database context</param>
     /// <param name="logger">Given Logger</param>
-    /// <param name="mapper">Given AutoMapper</param>
+    /// <param name="manager">Given SimpleSearchManager with data process methods</param>
     public SimpleSearchController(
         MediathequeDbContext context, 
         ILogger<SimpleSearchController> logger, 
-        IMapper mapper, 
         SimpleSearchManager manager
     )
     {
         _context = context;
         _logger = logger;
-        _mapper = mapper;
         _manager = manager;
     }
 
@@ -63,7 +54,7 @@ public class SimpleSearchController : ControllerBase
     [HttpPost]
     public async Task<IEnumerable<SearchResultDTO>> Post(SimpleSearchArgsDTO argsDto)
     {
-        if (argsDto?.Criterion == null || argsDto.Criterion == string.Empty) 
+        if (string.IsNullOrEmpty(argsDto?.Criterion))
         {
             return new List<SearchResultDTO>();
         }
@@ -71,22 +62,7 @@ public class SimpleSearchController : ControllerBase
         argsDto.Criterion = argsDto.Criterion.ToLower();
 
         // Criterion is searched into the title, the author name, the ISBN and the series' name
-        var books = _context.Books
-                            .Where(book => (
-                                                book.Title != null
-                                                && book.Title.ToLower().Contains(argsDto.Criterion)
-                                           )
-                                           || (
-                                                book.Author != null
-                                                && book.Author.CompleteName != null
-                                                && book.Author.CompleteName.ToLower().Contains(argsDto.Criterion)
-                                           )
-                                           || (
-                                                book.Editions != null
-                                                && book.Editions.Any(edition => edition.Isbn == argsDto.Criterion
-                                                   || (edition.Series != null && edition.Series.Name != null && edition.Series.Name.ToLower().Contains(argsDto.Criterion)))
-                                           )
-                                        );
+        var books = _context.Books.Where(_manager.GetSearchConditions(argsDto.Criterion));
 
         var results = await books.Include(b => b.Author)
                                  .Include(b => b.Genre)
@@ -98,25 +74,6 @@ public class SimpleSearchController : ControllerBase
                                  .ThenInclude(ed => ed.Publisher)
                                  .ToListAsync();
 
-        // Maps the books and the editions separately
-        // Orders the book by title's ascending alphabetical order
-        var bookResultDtos = _mapper.Map<List<Book>, List<BookResultDTO>>(results)
-                                    .OrderBy(book => book.Title);
-
-        var editionResultDtos = _mapper.Map<IEnumerable<Edition>, List<EditionResultDTO>>(results.SelectMany(res => res.Editions));
-
-        return bookResultDtos.Select(bookDto =>
-        {
-            // Binds each book to its editions and orders the editions by volume
-            var editions = _manager.OrderEditionsByVolume(
-                editionResultDtos.Where(edition => edition.BookId == bookDto.Id)
-            );
-
-            return new SearchResultDTO(bookDto)
-            {
-                // Editions have to be grouped by series' name
-                Editions = _manager.GroupEditionsBySeriesName(editions)
-            };
-        });
+        return _manager.GetSimpleSearchResults(results);
     }
 }
--- Managers/SearchManagers/SimpleSearchManager.cs
+++ Managers/SearchManagers/SimpleSearchManager.cs
@@ -1,5 +1,8 @@
-﻿using mediatheque_back_csharp.DTOs.SearchDTOs;
+﻿using AutoMapper;
+using mediatheque_back_csharp.DTOs.SearchDTOs;
 using mediatheque_back_csharp.Extensions;
+using mediatheque_back_csharp.Pocos;
+using System.Linq.Expressions;
 
 namespace mediatheque_back_csharp.Managers.SearchManagers;
 
@@ -9,11 +12,25 @@ namespace mediatheque_back_csharp.Managers.SearchManagers;
 public class SimpleSearchManager
 {
     /// <summary>
+    /// Transforms the POCOs into DTOs
+    /// </summary>
+    protected readonly IMapper _mapper;
+
+    /// <summary>
+    /// Constructor of the SimpleSearchManager class
+    /// </summary>
+    /// <param name="mapper">Given AutoMapper</param>
+    public SimpleSearchManager(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    /// <summary>
     /// Order the given list by volumes' alphanumerical ascending order
     /// </summary>
     /// <param name="editions">List of EditionResultDTO objects</param>
     /// <returns>Ordered list of EditionResultDTO objects</returns>
-    public List<EditionResultDTO> OrderEditionsByVolume(IEnumerable<EditionResultDTO> editions)
+    private List<EditionResultDTO> OrderEditionsByVolume(IEnumerable<EditionResultDTO> editions)
     {
         return editions.OrderBy(item => item.Volume.ExtractPrefix())
                        .ThenBy(item => item.Volume.ExtractNumber())
@@ -27,7 +44,7 @@ public class SimpleSearchManager
     /// <param name="editions">List of EditionResultDTOs objects</param>
     /// <returns>Returns a dictionary where the keys are the series' names
     /// and the elements are some lists containing editions</returns>
-    public Dictionary<string, List<EditionResultDTO>> GroupEditionsBySeriesName(IEnumerable<EditionResultDTO> editions)
+    private Dictionary<string, List<EditionResultDTO>> GroupEditionsBySeriesName(IEnumerable<EditionResultDTO> editions)
     {
         if (editions == null || editions.Count() == 0)
         {
@@ -44,5 +61,69 @@ public class SimpleSearchManager
             return "0";
 
         }).ToDictionary(group => group.Key, group => group.ToList());
+    }
+
+    /// <summary>
+    /// Returns the expression used for searching the books into the DbContext.
+    /// </summary>
+    /// <param name="criterion">Title, author name, ISBN or series' name</param>
+    /// <returns>Returns an expression which receive a Book as a parameter
+    /// and returns boolean expressions</returns>
+    public Expression<Func<Book, bool>> GetSearchConditions(string criterion)
+    {
+        if (string.IsNullOrEmpty(criterion))
+        {
+            return book => true;
+        }
+
+        return book => (
+            book.Title != null
+            && book.Title.ToLower().Contains(criterion)
+        )
+        || (
+            book.Author != null
+            && book.Author.CompleteName != null
+            && book.Author.CompleteName.ToLower().Contains(criterion)
+        )
+        || (
+            book.Editions != null
+            && book.Editions.Any(edition => edition.Isbn == criterion
+                || (edition.Series != null && edition.Series.Name != null && edition.Series.Name.ToLower().Contains(criterion)))
+        );
+    }
+
+    /// <summary>
+    /// Constructs the final results list from the given books list
+    /// </summary>
+    /// <param name="books">Books sent by the database</param>
+    /// <returns>List of SearchResultDTOs (BookId + Book object + Editions grouped by series' name)</returns>
+    public IEnumerable<SearchResultDTO> GetSimpleSearchResults(List<Book> books)
+    {
+        if (books == null) { 
+            return Enumerable.Empty<SearchResultDTO>(); 
+        }
+
+        // Maps the books and the editions separately
+        // Orders the book by title's ascending alphabetical order
+        var bookResultDtos = _mapper.Map<List<Book>, List<BookResultDTO>>(books)
+                                    .OrderBy(book => book.Title);
+
+        var editionResultDtos = _mapper.Map<IEnumerable<Edition>, List<EditionResultDTO>>(
+            books.SelectMany(res => res.Editions)
+        );
+
+        return bookResultDtos.Select(bookDto =>
+        {
+            // Binds each book to its editions and orders the editions by volume
+            var editionsForABook = OrderEditionsByVolume(
+                editionResultDtos.Where(edition => edition.BookId == bookDto.Id)
+            );
+
+            return new SearchResultDTO(bookDto)
+            {
+                // Editions have to be grouped by series' name
+                Editions = GroupEditionsBySeriesName(editionsForABook)
+            };
+        });
     }
 }
