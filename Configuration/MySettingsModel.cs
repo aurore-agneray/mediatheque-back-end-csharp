867ce663c8ccfffc6aa0c2e1cc8339330dd91bbf
aurore-agneray <aurore.agn@outlook.fr>
2024-07-10 10:44:07 +0200

    Déplacement des fichiers MySettingsModel et ServicesOptions dans un nouveau dossier "Configuration"

---
+++ Configuration/MySettingsModel.cs
@@ -0,0 +1,18 @@
+﻿namespace mediatheque_back_csharp.Configuration;
+
+/// <summary>
+/// Object used to retrieve the settings from the appsettings.json file
+/// </summary>
+public class MySettingsModel
+{
+    /// <summary>
+    /// Connection string used for connecting to the database, that depends on the execution environment
+    /// </summary>
+    public string? DbConnectionString { get; set; }
+
+    /// <summary>
+    /// Domain(s) of the front-end that will call the API.
+    /// If several domains, separate them with ;
+    /// </summary>
+    public string? FrontEndDomains { get; set; }
+}
