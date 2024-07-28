1d5661047695d1901e761999142138fc337f1f32
Aurore Agneray <aurore.agn@outlook.fr>
2024-07-28 14:51:28 +0200

    Mise en place d'un filtre dans les paramètres du Swagger pour masquer les routes du SearchController

---
+++ Configuration/CustomSwaggerFilter.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace mediatheque_back_csharp.Configuration;
+
+/// <summary>
+/// Defines which functionalities must appear or not into the Swagger
+/// </summary>
+public class CustomSwaggerFilter : IDocumentFilter
+{
+    /// <summary>
+    /// Defines which functionalities must appear or not into the Swagger
+    /// </summary>
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var controllersToHide = new[] { "Search" };
+
+        /* The object ApiDescriptions describes the content of the C# controllers' classes. */
+        var pathsToHide = context.ApiDescriptions
+            .Where(desc => controllersToHide.Contains(desc.ActionDescriptor.RouteValues["controller"]))
+            .Select(desc => "/" + desc.RelativePath.TrimEnd('/'))
+            .ToList();
+
+        pathsToHide.ForEach(path => swaggerDoc.Paths.Remove(path));
+
+        foreach (var path in pathsToHide)
+        {
+            swaggerDoc.Paths.Remove(path);
+        }
+    }
+}
