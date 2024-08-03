84b6c7007f2e79235545bcbd1edffff61c8f2242
aurore-agneray <aurore.agn@outlook.fr>
2024-08-03 11:49:52 +0200

    Installation du package Linqkit et mise en place de la requête avancée avec des conditions séparées par OR. A FINIR !! Manque la condition de date de publication

--- Managers/SearchManagers/AdvancedSearchManager.cs
+++ Managers/SearchManagers/AdvancedSearchManager.cs
@@ -1,6 +1,10 @@
 using AutoMapper;
+using LinqKit;
 using mediatheque_back_csharp.Database;
 using mediatheque_back_csharp.DTOs.SearchDTOs;
+using mediatheque_back_csharp.Pocos;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace mediatheque_back_csharp.Managers.SearchManagers;
 
@@ -19,21 +23,103 @@ public class AdvancedSearchManager : SearchManager
     }
 
     /// <summary>
+    /// Generate the IQueryable object dedicated to 
+    /// retrieve the books from the database,
+    /// ordered by the title
+    /// </summary>
+    /// <param name="criteria">Criteria sent by the client</param>
+    /// <returns>A IQueryable<Book> object</returns>
+    private IQueryable<Book> GetOrderedBooksRequest(SearchArgsDTO criteria)
+    {
+        if (criteria == null)
+        {
+            return default;
+        }
+
+        IQueryable<Book> query = (
+            from boo in _context.Books
+            join genre in _context.Genres on boo.GenreId equals genre.Id
+            join author in _context.Authors on boo.AuthorId equals author.Id
+            join ed in _context.Editions on boo.Id equals ed.BookId
+            join publisher in _context.Publishers on ed.PublisherId equals publisher.Id
+            join format in _context.Formats on ed.FormatId equals format.Id
+            join series in _context.Series on ed.SeriesId equals series.Id into seriesJoin
+            from series in seriesJoin.DefaultIfEmpty() // Retrieves editions even if they have no series
+            select boo
+        )
+        .Distinct()
+        .OrderBy(b => b.Title);
+
+        var expression = PredicateBuilder.New<Book>(false);
+
+        if (!string.IsNullOrEmpty(criteria?.Title))
+        {
+            expression = expression.Or(book =>
+                book.Title != null && book.Title.Contains(criteria.Title)
+            );
+        }
+
+        if (!string.IsNullOrEmpty(criteria?.Isbn))
+        {
+            expression = expression.Or(book =>
+                book.Editions.Any(ed => ed.Isbn == criteria.Isbn)
+            );
+        }
+
+        if (criteria?.Publisher?.Id != null)
+        {
+            expression = expression.Or(book =>
+                book.Editions.Any(ed => ed.PublisherId == criteria.Publisher.Id)
+            );
+        }
+
+        if (!string.IsNullOrEmpty(criteria?.Series))
+        {
+            expression = expression.Or(book =>
+                book.Editions.Any(ed =>
+                    ed.Series != null && ed.Series.Name != null && ed.Series.Name.Contains(criteria.Series)
+                )
+            );
+        }
+
+        if (criteria?.Format?.Id != null)
+        {
+            expression = expression.Or(book =>
+                book.Editions.Any(ed => ed.FormatId == criteria.Format.Id)
+            );
+        }
+
+        if (!string.IsNullOrEmpty(criteria?.Author))
+        {
+            expression = expression.Or(book =>
+                book.Author != null && book.Author.CompleteName != null && book.Author.CompleteName.Contains(criteria.Author)
+            );
+        }
+
+        if (criteria?.Genre?.Id != null)
+        {
+            expression = expression.Or(book => book.Genre != null && book.Genre.Id == criteria.Genre.Id);
+        }
+
+        return query.Where(expression);
+    }
+
+    /// <summary>
     /// Processes the advanced search
     /// </summary>
     /// <param name="criteria">Object containing the search criteria</param>
     /// <returns>List of some SearchResultsDTO objects</returns>
     public override async Task<List<SearchResultDTO>> SearchForResults(SearchArgsDTO criteria)
     {
-        return await Task.Run(async() => {
-            var test = criteria?.Title;
-            return new List<SearchResultDTO>() {
-                new SearchResultDTO(new BookResultDTO() {
-                    Title = test
-                }) {
-                    BookId = 1
-                }
-            };
-        });
+        var query = GetOrderedBooksRequest(criteria);
+
+        if (query != null)
+        {
+            var asyncList = await query.ToListAsync();
+            var dtos = _mapper.Map<List<BookResultDTO>>(asyncList);
+            return dtos.Select(dto => new SearchResultDTO(dto)).ToList();
+        }
+
+        return new List<SearchResultDTO>();
     }
 }
