aba1e2ef11e154716d8c3e810e1de3485c674ceb
Aurore Agneray <aurore.agn@outlook.fr>
2024-08-04 15:04:45 +0200

    Suppression de la classe ExpressionStarterBookExtensions précédemment créée (ne me semblait finalement pas pertinente) + découpage du code dans AdvancedSearchManager

--- Managers/SearchManagers/AdvancedSearchManager.cs
+++ Managers/SearchManagers/AdvancedSearchManager.cs
@@ -2,7 +2,6 @@ using AutoMapper;
 using LinqKit;
 using mediatheque_back_csharp.Database;
 using mediatheque_back_csharp.DTOs.SearchDTOs;
-using mediatheque_back_csharp.Extensions;
 using mediatheque_back_csharp.Pocos;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,36 +22,53 @@ public class AdvancedSearchManager : SearchManager
     }
 
     /// <summary>
-    /// Generate the IQueryable object dedicated to 
-    /// retrieve the books from the database,
-    /// ordered by the title
+    /// Completes the concerned Expression with the condition on the publication date
     /// </summary>
-    /// <param name="criteria">Criteria sent by the client</param>
-    /// <returns>A IQueryable<Book> object</returns>
-    private IQueryable<Book> GetOrderedBooksRequest(SearchArgsDTO criteria)
+    /// <param name="expression">ExpressionStarter<Book> object</param>
+    /// <param name="op">Operator ("=", "<", ">")</param>
+    /// <param name="criterionDate">Date used for comparison with the database</param>
+    /// <returns>Returns the completed expression</returns>
+    /// <exception cref="ArgumentException">Occured when the operator has an inappropriate value</exception>
+    private ExpressionStarter<Book> AddPublicationDateCondition(ExpressionStarter<Book> expression, string op, DateTime criterionDate)
     {
-        if (criteria == null)
+        /* WARNING : I'd like to factorize more this part but it seems impossible because of the request translation */
+        switch (op)
         {
-            return default;
+            case "=":
+                expression = expression.Or(book => 
+                    book.Editions.Any(ed => 
+                        (ed.PublicationDate.HasValue && ed.PublicationDate.Value.Date == criterionDate.Date)
+                        || (ed.PublicationYear.HasValue && ed.PublicationYear.Value == criterionDate.Year)
+                    ));
+                break;
+            case "<":
+                expression = expression.Or(book => 
+                    book.Editions.Any(ed => 
+                        (ed.PublicationDate.HasValue && ed.PublicationDate.Value.Date < criterionDate.Date)
+                        || (ed.PublicationYear.HasValue && ed.PublicationYear.Value < criterionDate.Year)
+                    ));
+                break;
+            case ">":
+                expression = expression.Or(book => 
+                    book.Editions.Any(ed => 
+                        (ed.PublicationDate.HasValue && ed.PublicationDate.Value.Date > criterionDate.Date)
+                        || (ed.PublicationYear.HasValue && ed.PublicationYear.Value > criterionDate.Year)
+                    ));
+                break;
+            default:
+                throw new ArgumentException("Invalid operator for dates' comparison", nameof(op));
         }
-        
-        DateTime criterionDate;
 
-        IQueryable<Book> query = (
-            from boo in _context.Books
-            join genre in _context.Genres on boo.GenreId equals genre.Id
-            join author in _context.Authors on boo.AuthorId equals author.Id
-            join ed in _context.Editions on boo.Id equals ed.BookId
-            join publisher in _context.Publishers on ed.PublisherId equals publisher.Id
-            join format in _context.Formats on ed.FormatId equals format.Id
-            join series in _context.Series on ed.SeriesId equals series.Id into seriesJoin
-            from series in seriesJoin.DefaultIfEmpty() // Retrieves editions even if they have no series
-            select boo
-        )
-        .Distinct()
-        .OrderBy(b => b.Title);
+        return expression;
+    }
 
-        var expression = PredicateBuilder.New<Book>(false);
+    /// <summary>
+    /// Completes the concerned Expression with the condition on the book object and its properties,
+    /// except the editions
+    /// </summary>
+    /// <param name="expression">ExpressionStarter<Book> object</param>
+    /// <param name="criteria">Criteria sent by the client</param>
+    private void AddBookConditions(ref ExpressionStarter<Book> expression, SearchArgsDTO criteria) {
 
         if (!string.IsNullOrEmpty(criteria?.Title))
         {
@@ -60,6 +76,28 @@ public class AdvancedSearchManager : SearchManager
                 book.Title != null && book.Title.Contains(criteria.Title)
             );
         }
+
+        if (!string.IsNullOrEmpty(criteria?.Author))
+        {
+            expression = expression.Or(book =>
+                book.Author != null && book.Author.CompleteName != null && book.Author.CompleteName.Contains(criteria.Author)
+            );
+        }
+
+        if (criteria?.Genre?.Id != null)
+        {
+            expression = expression.Or(book => book.Genre != null && book.Genre.Id == criteria.Genre.Id);
+        }
+    }
+
+    /// <summary>
+    /// Completes the concerned Expression with the condition on the editions and their properties
+    /// </summary>
+    /// <param name="expression">ExpressionStarter<Book> object</param>
+    /// <param name="criteria">Criteria sent by the client</param>
+    private void AddEditionsConditions(ref ExpressionStarter<Book> expression, SearchArgsDTO criteria) {
+        
+        DateTime criterionDate;
 
         if (!string.IsNullOrEmpty(criteria?.Isbn))
         {
@@ -95,20 +133,42 @@ public class AdvancedSearchManager : SearchManager
             && !string.IsNullOrEmpty(criteria?.PubDate?.Operator)
             && DateTime.TryParse(criteria.PubDate.Criterion, out criterionDate)) 
         {
-            expression = expression.AddPublicationDateCondition(criteria.PubDate.Operator, criterionDate);
+            expression = AddPublicationDateCondition(expression, criteria.PubDate.Operator, criterionDate);
+        }
+    }
+
+    /// <summary>
+    /// Generate the IQueryable object dedicated to 
+    /// retrieve the books from the database,
+    /// ordered by the title
+    /// </summary>
+    /// <param name="criteria">Criteria sent by the client</param>
+    /// <returns>A IQueryable<Book> object</returns>
+    private IQueryable<Book> GetOrderedBooksRequest(SearchArgsDTO criteria)
+    {
+        if (criteria == null)
+        {
+            return default;
         }
 
-        if (!string.IsNullOrEmpty(criteria?.Author))
-        {
-            expression = expression.Or(book =>
-                book.Author != null && book.Author.CompleteName != null && book.Author.CompleteName.Contains(criteria.Author)
-            );
-        }
+        IQueryable<Book> query = (
+            from boo in _context.Books
+            join genre in _context.Genres on boo.GenreId equals genre.Id
+            join author in _context.Authors on boo.AuthorId equals author.Id
+            join ed in _context.Editions on boo.Id equals ed.BookId
+            join publisher in _context.Publishers on ed.PublisherId equals publisher.Id
+            join format in _context.Formats on ed.FormatId equals format.Id
+            join series in _context.Series on ed.SeriesId equals series.Id into seriesJoin
+            from series in seriesJoin.DefaultIfEmpty() // Retrieves editions even if they have no series
+            select boo
+        )
+        .Distinct()
+        .OrderBy(b => b.Title);
 
-        if (criteria?.Genre?.Id != null)
-        {
-            expression = expression.Or(book => book.Genre != null && book.Genre.Id == criteria.Genre.Id);
-        }
+        var expression = PredicateBuilder.New<Book>(false);
+
+        AddBookConditions(ref expression, criteria);
+        AddEditionsConditions(ref expression, criteria);
 
         return query.Where(expression);
     }
