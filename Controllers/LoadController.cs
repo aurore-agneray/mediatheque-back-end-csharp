0ad33c39b191f6366e452179f8f5c61b4aec48a9
aurore-agneray <aurore.agn@outlook.fr>
2024-08-19 17:59:18 +0200

    Ajustements dans LoadController avec ajout d'un try / catch et renvoi d'une erreur 500 avec message spécifique si la base de données n'est pas accessible

--- Controllers/LoadController.cs
+++ Controllers/LoadController.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using mediatheque_back_csharp.Classes;
 using mediatheque_back_csharp.Database;
 using mediatheque_back_csharp.Dtos;
 using mediatheque_back_csharp.Interfaces;
+using mediatheque_back_csharp.Texts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Resources;
 
 namespace mediatheque_back_csharp.Controllers;
 
@@ -31,20 +35,28 @@ public class LoadController : ControllerBase
     protected readonly IMapper _mapper;
 
     /// <summary>
+    /// Gives access to the texts of the app
+    /// </summary>
+    protected readonly ResourceManager _textsManager;
+
+    /// <summary>
     /// Constructor of the LoadController class
     /// </summary>
     /// <param name="context">Given database context</param>
     /// <param name="logger">Given Logger</param>
     /// <param name="mapper">Given AutoMapper</param>
+    /// <param name="textsManager">Texts manager</param>
     public LoadController(
         MediathequeDbContext context,
         ILogger<LoadController> logger,
-        IMapper mapper
+        IMapper mapper, 
+        ResourceManager textsManager
     )
     {
         _context = context;
         _logger = logger;
         _mapper = mapper;
+        _textsManager = textsManager;
     }
 
     /// <summary>
@@ -53,16 +65,25 @@ public class LoadController : ControllerBase
     /// </summary>
     /// <returns>Returns a DTO with several lists</returns>
     [HttpGet]
-    public async Task<LoadDTO> Get()
+    public async Task<IActionResult> Get()
     {
-        return await Task.Run(async() => {    
+        Task<List<NamedDTO>> ProjectDbINamedList(IOrderedQueryable<INamed> list)
+        {
+            return list.ProjectTo<NamedDTO>(_mapper.ConfigurationProvider)
+                       .ToListAsync();
+        }
 
-            Task<List<NamedDTO>> ProjectDbINamedList(IOrderedQueryable<INamed> list) {
-                return list.ProjectTo<NamedDTO>(_mapper.ConfigurationProvider)
-                           .ToListAsync();
-            }
+        if (_context == null || !_context.Database.CanConnect())
+        {
+            return new ErrorObject(
+                HttpStatusCode.InternalServerError, 
+                _textsManager.GetString(TextsKeys.ERROR_DATABASE_CONNECTION)
+            );
+        }
 
-            return new LoadDTO
+        try
+        {
+            var results = new LoadDTO
             {
                 Genres = await ProjectDbINamedList(_context.Genres.OrderBy(g => g.Name)),
                 Publishers = await _context.Publishers.OrderBy(p => p.PublishingHouse)
@@ -70,6 +91,12 @@ public class LoadController : ControllerBase
                                                       .ToListAsync(),
                 Formats = await ProjectDbINamedList(_context.Formats.OrderBy(f => f.Name))
             };
-        });
+
+            return Ok(results);
+        }
+        catch (Exception ex)
+        {
+            return new ErrorObject(HttpStatusCode.InternalServerError, ex.Message);
+        }
     }
 }
