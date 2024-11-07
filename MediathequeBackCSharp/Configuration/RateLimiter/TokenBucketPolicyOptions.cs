331e21caa0559ff92b94257b732fb603321dee24
Aurore Agneray <aurore.agn@outlook.fr>
2024-11-07 14:50:13 +0100

    - Modification du niveau d'accès de la classe TokenBucketPolicyOptions (internal --> public) afin que la lecture du fichier de configuration fonctionne - Mise en place de la lecture du fichier de config dans MyRateLimiterOptions - Ajout de messages d'erreur dans InternalErrorTexts.cs et FrTexts.resx (absence des taux du rate limiter dans appsettings et information utilisateur en cas de surcharge d'appels)

--- MediathequeBackCSharp/Configuration/RateLimiter/TokenBucketPolicyOptions.cs
+++ MediathequeBackCSharp/Configuration/RateLimiter/TokenBucketPolicyOptions.cs
@@ -3,25 +3,25 @@
 /// <summary>
 /// Defines the options of a given rate limiter policy of type "Token Bucket"
 /// </summary>
-internal class TokenBucketPolicyOptions
+public class TokenBucketPolicyOptions
 {
     /// <summary>
     /// The maximum quantity of available tokens
     /// </summary>
-    internal int TokenLimit { get; set; }
+    public int TokenLimit { get; set; }
     
     /// <summary>
     /// The maximum quantity of requests into the queue
     /// </summary>
-    internal int QueueLimit { get; set; }
+    public int QueueLimit { get; set; }
 
     /// <summary>
     /// The period of time that has to be waited for gettings new tokens
     /// </summary>
-    internal int ReplenishmentPeriod { get; set; }
+    public int ReplenishmentPeriod { get; set; }
 
     /// <summary>
     /// The number of new got tokens within each replenishment period
     /// </summary>
-    internal int TokensPerPeriod { get; set; }
+    public int TokensPerPeriod { get; set; }
 }
--- MediathequeBackCSharp/Texts/TextsKeys.cs
+++ MediathequeBackCSharp/Texts/TextsKeys.cs
@@ -13,6 +13,7 @@ public class TextsKeys
     public const string ERROR_DATABASE_CONNECTION = "ERROR_DATABASE_CONNECTION";
     public const string ERROR_MISSING_CRITERIA = "ERROR_MISSING_CRITERIA";
     public const string ERROR_NO_IMPLEMENTED_SEARCH_TYPE = "ERROR_NO_IMPLEMENTED_SEARCH_TYPE";
+    public const string ERROR_RATE_LIMIT_REACHED = "ERROR_RATE_LIMIT_REACHED";
     public const string SIMPLE_SEARCH_TYPE = "SIMPLE_SEARCH_TYPE";
 
 #pragma warning restore CS1591
